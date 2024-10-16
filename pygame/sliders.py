ca0615ae4b0889b122dc482c31e73ab61d483414
nihaltirunami <nihal.tirumani@gmail.com>
2024-10-16 20:49:16 +0530

    sliders rounded slider filling fix

--- pygame/sliders.py
+++ pygame/sliders.py
@@ -25,6 +25,9 @@ slider1 = pygame.Rect(98, 90, width1, height1)
 restrisction1 = 100 - height / 2
 restrisction2 = 400 - height / 2
 
+restrisction1_slider1 = restrisction1 + 7
+restrisction2_slider1 = restrisction2 + 7
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,6 +36,7 @@ while True:
 
     screen.fill((220, 220, 220))
 
+    #Slider button touching collisions
     mouse = pygame.mouse.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
 
@@ -44,27 +48,33 @@ while True:
         slider1.y = mouse_pos[1] - 12.5
         old_mouse_pressed1 = mouse[0]
 
+    # Adding restrictions to slider buttons
     if slider.y < restrisction1: slider.y = restrisction1
     elif slider.y > restrisction2: slider.y = restrisction2
 
-    if slider1.centery < restrisction1 + 7: slider1.centery = restrisction1 + 7
-    elif slider1.centery > restrisction2 + 7: slider1.centery = restrisction2 + 7
+    if slider1.centery < restrisction1_slider1: slider1.centery = restrisction1_slider1
+    elif slider1.centery > restrisction2_slider1: slider1.centery = restrisction2_slider1
 
-    pygame.draw.line(screen, "white", (50, 410), (50, 90), 40) # draws down to up
-    pygame.draw.line(screen, "#4664fa", (50, 400), (50, slider.centery), 20) # draws down to up
-    pygame.draw.rect(screen, "#3858fc", slider)
+    # Uses line method to draw a slider except the slider button.
+    pygame.draw.line(screen, "white", (50, 410), (50, 90), 40) # draws down to up (outer part)
+    pygame.draw.line(screen, "#4664fa", (50, 400), (50, slider.centery), 20) # draws down to up (filled part)
+    pygame.draw.rect(screen, "#3858fc", slider) # slider button
 
-    pygame.draw.rect(screen, "white", (90, 80, 40, 340), border_radius = 20)
-    pygame.draw.rect(screen, "#4664fa", (100, slider1.y, 20, (3.2 * (100 - (slider1.centery - 102) // 3))), border_radius = 12)
-    pygame.draw.ellipse(screen, "#3858fc", slider1)
+    # Uses rect method to draw a slider except the slider button which uses ellipse method to draw.
+    pygame.draw.rect(screen, "white", (90, 80, 40, 340), border_radius = 20) # slider1 rect (outer part)
+    filling_height = (100, slider1.centery, 20, 310 - (slider1.centery - (restrisction1 + 7)))
+    pygame.draw.rect(screen, "#4664fa", filling_height, border_radius = 12) # slider1 rect (filled part)
+    pygame.draw.ellipse(screen, "#3858fc", slider1) # slider1 button
 
+    # Value of rectangle (line) slider
     value = 100 - floor((slider.centery - 100 ) / 3 )
-    text = font.render(f"Value: {value}", True, "black")
+    text = font.render(f"Value1: {value}", True, "black")
     text_rect = text.get_rect(topleft = (10, 10))
     screen.blit(text, text_rect)
 
+    # Value of rounded (rect) slider
     value1 = 100 - (slider1.centery - 102) // 3
-    text1 = font.render(f"Value: {value1}", True, "black")
+    text1 = font.render(f"Value2: {value1}", True, "black")
     text_rect1 = text.get_rect(topleft = (300, 10))
     screen.blit(text1, text_rect1)
 
