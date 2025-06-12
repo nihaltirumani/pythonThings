2bdda418db3584542b6130396f6bc372ba80383f
Nihal Tirumani <nihal.tirumani@gmail.com>
2025-06-12 17:59:22 +0530

    Countdown timer and minor changes

---
+++ smallProjects/countdownTimer.py
@@ -0,0 +1,8 @@
+import time
+
+countdown_time = int(input("Enter countdown time in seconds: "))
+
+time.sleep(1)
+print("Countdown started!")
+time.sleep(countdown_time)
+print("Time's up!")
--- testers/aTestProject.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-number = ""
-for i in range(6): number += str(randint(0,9))
-print(number)
